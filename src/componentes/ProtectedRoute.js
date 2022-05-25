fffece2bce8f3bf1e27989cb99ad4cbab9d88f51
angiephb <angie.pio30@gmail.com>
2022-05-24 20:27:53 -0500

    añadiendo stilos a login con bootstrap)

--- src/componentes/ProtectedRoute.js
+++ src/componentes/ProtectedRoute.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Navigate, Route } from "react-router-dom";
+// import Login from './login/login.js'
+// import Waiter from './rol/waiter/waiter.js'
+// import Chef from './rol/chef/chef.js'
+// import Admin from '../rol/admin/admin.js';
 
 function ProtectedRoute({ component: Component, ...restOfProps }) {
-  const isRol = localStorage.getItem("Rol");
+  const isRol = localStorage.getItem('Rol');
   console.log("this", isRol);
 
   return (
@@ -10,7 +14,7 @@ function ProtectedRoute({ component: Component, ...restOfProps }) {
       {...restOfProps}
       render={(Admin) =>
 
-        isRol=== "Admin" ? <Component {...Admin} /> : <Navigate to="/admin" />
+        isRol === "admin" ? <Component {...Admin} /> : <Navigate to="/" />
       }
     />
   );
