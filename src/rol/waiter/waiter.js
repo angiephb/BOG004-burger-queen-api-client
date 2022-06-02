518c7d780d227d77555c4fe36a5446bc1bf8127c
gilliam24 <gilliamargelmonterroza@gmail.com>
2022-06-02 13:18:17 -0500

    corregimos error del botón logout y ya hace la navegación hacia la vista login

--- src/rol/waiter/waiter.js
+++ src/rol/waiter/waiter.js
@@ -8,18 +8,19 @@ const Waiter = () => {
             </header>
             <section className='btnWaiter'>
                 <p className='gretting'>Hola Mesero, </p>
+            <form>
 
+     </form>
                 <aside className='orderbtn'>
                     <div className='btns'>
-                    <button type="button" className='btn btn-default btn-circle'><i className="fa-solid fa-plus"></i>
+                    <button type="button" className='btn btn-default btn-circle'><i className='fa-solid fa-plus'></i>
                     </button>
                     <label className="labelOrder">Nueva Orden</label>
                     </div>
                     <div className='btns'>
-                    <button type="button" className='btn btn-default btn-circle'><i class="fa-solid fa-clipboard-list"></i>
-
+                    <button type="button" className='btn btn-default btn-circle'><i className='fa-solid fa-clipboard-list'></i>
                     </button>
-                    <label className="labelOrder">Tus Ordenes</label>
+                    <label className='labelOrder'>Tus Ordenes</label>
                     </div>
                 </aside>
             </section>
