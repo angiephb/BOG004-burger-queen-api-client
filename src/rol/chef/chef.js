5dd6dcdef117627f06b6b4db21292e056f6396eb
angiephb <angie.pio30@gmail.com>
2022-06-16 08:06:55 -0500

    maquetando vista chef

--- src/rol/chef/chef.js
+++ src/rol/chef/chef.js
@@ -1,10 +1,26 @@
-
+import HeaderView from "../../componentes/HeaderView.js";
 
 const Chef = () => {
     return (
-        <h1>
-            Chef
-        </h1>
+        <main>
+            <header>
+                <HeaderView />
+            </header>
+            <section>
+                <p className='gretting'>Hola Chef, </p>
+            </section>
+            <section className='containerOrders'>
+                <section className='btnOrders'>
+                    <section>
+                        <button type='button' className='btn btn-order' >Pendientes</button>
+                    </section>
+
+                    <section>
+                        <button type='button' className='btn btn-order' >Entregadas</button>
+                    </section>
+                </section>
+            </section>
+        </main>
     );
 
 }
